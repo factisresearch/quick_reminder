fb56558afbfb17fe477d22a870c6180f86e1b1bc
Stefan Wehr <wehr@cp-med.com>
2019-08-22 23:52:44 +0200

    integrate dateTime picker

--- lib/input.dart
+++ lib/input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:quick_reminder/button.dart';
 import "./buttonGroup.dart";
 
@@ -12,11 +13,58 @@ class Input extends StatefulWidget {
 
 class _Input extends State<Input> {
 
+  int _selectedDayIndex = -1;
+  int _selectedTimeIndex = -1;
+  DateTime _explicitedDateTime = null;
+
   List<String> dayButtons = ["heute", "morgen", "Fr", "Sa", "So", "Mo"];
   List<String> timeButtons = ["7:00", "11:00", "15:00", "19:00", "22:00"];
 
   @override
   Widget build(BuildContext context) {
+    Widget quickButtonsOrText;
+    if (_explicitedDateTime != null) {
+      quickButtonsOrText = Text(_explicitedDateTime.toString());
+    } else {
+      quickButtonsOrText =
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ButtonGroup(
+              callback: (index, enabled) {
+                setState(() {
+                  if (enabled) {
+                    _selectedDayIndex = index;
+                  } else {
+                    _selectedDayIndex = -1;
+                  }
+                });
+                print("Button $index is now ${enabled ? 'enabled' : 'disabled'}");
+              },
+              buttonAttributes: dayButtons.map((text) {
+                return new ButtonAttributes(text: text);
+              }).toList()
+            ),
+            const SizedBox(height: 12),
+            ButtonGroup(
+              callback: (index, enabled) {
+                setState(() {
+                  if (enabled) {
+                    _selectedTimeIndex = index;
+                  } else {
+                    _selectedTimeIndex = -1;
+                  }
+                });
+                print("Button $index is now ${enabled ? 'enabled' : 'disabled'}");
+              },
+              buttonAttributes: timeButtons.map((text) {
+                return new ButtonAttributes(text: text);
+              }).toList()
+            ),
+          ]
+        );
+    }
     return Column(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -25,34 +73,23 @@ class _Input extends State<Input> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ButtonGroup(
-                  callback: (index, enabled) {
-                    print("Button $index is now ${enabled ? 'enabled' : 'disabled'}");
-                  },
-                  buttonAttributes: dayButtons.map((text) {
-                    return new ButtonAttributes(text: text);
-                  }).toList()
-                ),
-                const SizedBox(height: 12),
-                ButtonGroup(
-                  callback: (index, enabled) {
-                    print("Button $index is now ${enabled ? 'enabled' : 'disabled'}");
-                  },
-                  buttonAttributes: timeButtons.map((text) {
-                    return new ButtonAttributes(text: text);
-                  }).toList()
-                ),
-              ]
-            ),
+            quickButtonsOrText,
             const SizedBox(width: 20),
             Button(
               buttonAttributes: new ButtonAttributes(text: ">>"),
               callback: (bool enabled) {
-                print("hit >>");
+                DatePicker.showDateTimePicker(
+                  context,
+                  showTitleActions: true,
+                  onConfirm: (date) {
+                    setState(() {
+                      _explicitedDateTime = date;
+                    });
+                    print('confirm $date');
+                  },
+                  currentTime: DateTime.now(),
+                  locale: LocaleType.de
+                );
               },
             )
           ]
@@ -64,9 +101,6 @@ class _Input extends State<Input> {
             Expanded(
               child:
                 Container(
-                  //height: 36.0,
-                  //width: 300,
-                  //padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 8.0),
                   margin: const EdgeInsets.symmetric(horizontal: 5.0),
                   child: TextField(
                     cursorColor: Colors.black,
@@ -74,8 +108,14 @@ class _Input extends State<Input> {
                 ),
             ),
             FloatingActionButton(
-              onPressed: () { print("Pressed +"); },
-              tooltip: 'Increment',
+              onPressed: () {
+                setState(() {
+                  _explicitedDateTime = null;
+                  _selectedDayIndex = -1;
+                  _selectedTimeIndex = -1;
+                });
+                print("Pressed +");
+              },
               child: Icon(Icons.add),
               mini: true,
             ),
